0aeb9abdeefad7e51892439481cb0ae3ee051c1e
joe-bor <kimborlongan_08@yahoo.com>
2023-12-28 23:19:52 -0800

    added description beside barcodes:

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ dfs = tabula.read_pdf("iris.pdf", pages='all')
 print(type(dfs))
 print(len(dfs))
 
-# -------------------------------------------------
+# ------------GENERATE BARCODES -------------------------------
 
 # Create a directory to save barcodes
 os.makedirs('barcodes', exist_ok=True)
@@ -19,20 +19,28 @@ def generate_barcode(upc_code, filename):
     upc = UPC(upc_code, writer=SVGWriter())
     upc.save(filename)
 
+# Dictionary to store product number and its description
+product_descriptions = {}
+
 for df in dfs:
     # Selecting only required columns (up to UPC)
     df = df[['Product', 'Order', 'Description', 'Code', 'UPC']]
 
-    # Generating barcodes for each product
+    # Generating barcodes for each product and storing descriptions
     for index, row in df.iterrows():
         product_number = str(row['Product'])
+        description = row['Description']
+
         # Ensure product number is 12 digits for UPC-A
         product_number = product_number.zfill(12)
         
         barcode_file = f"barcodes/{product_number}"
         generate_barcode(product_number, barcode_file)
 
+        # Store the description in the dictionary
+        product_descriptions[product_number] = description
 
+# ----------- GENERATE HTML ----------
 
 html_content = "<html><body>"
 
@@ -42,14 +50,21 @@ barcode_dir = 'barcodes'
 # Loop through each file in the directory
 for filename in os.listdir(barcode_dir):
     if filename.endswith(".svg"):
+        # Extract the product number from the filename
+        product_number = filename.split('.')[0]
+        
+        # Get the description from the dictionary
+        description = product_descriptions.get(product_number, "No description")
+
         # Construct the path to the barcode file
         filepath = os.path.join(barcode_dir, filename)
         
-        # Add an img tag for the barcode
-        html_content += f'<img src="{filepath}" alt="{filename}"><br>'
+        # Add an img tag for the barcode and the description next to it
+        html_content += f'<img src="{filepath}" alt="{filename}"> {description}<br>'
 
 html_content += "</body></html>"
 
 # Write the HTML content to a file
 with open("barcodes.html", "w") as html_file:
     html_file.write(html_content)
+
