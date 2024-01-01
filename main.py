873ba75d7554ab84bcc50bd4d89ec38e07d7ae07
joe-bor <kimborlongan_08@yahoo.com>
2024-01-01 08:52:39 -0800

    outlined TODOs

--- main.py
+++ main.py
@@ -107,3 +107,11 @@ html_content += """
 # Write the HTML content to a file
 with open("barcodes.html", "w") as html_file:
     html_file.write(html_content)
+
+
+# TODO: Create frontend to handle file upload
+    # TODO: Add styles + Instructions
+# TODO: Tweak backend logic to handle file upload, instead of local pdf file
+    # TODO: Validation? Perhaps to prevent app from crashing.
+# TODO: Delete SVGs for next person
+# TODO: Delete barcodes.html? Or maybe keep it since it'll get replaced anyway
