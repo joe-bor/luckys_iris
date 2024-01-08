75b4ae4f1905d8b9a5ecaf2be8d133277517aeea
joe-bor <kimborlongan_08@yahoo.com>
2024-01-07 16:11:50 -0800

    fix server

--- server.py
+++ server.py
@@ -7,4 +7,4 @@ if __name__ == "__name__":
     
     port = int(os.getenv('PORT', 8000))
     
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
